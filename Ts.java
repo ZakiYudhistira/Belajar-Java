7a91618531e1391a0e10773d62c6c3cbea8b8e36
zaki <zakiyudhistira.itb69@gmail.com>
2023-09-28 20:35:52 +0700

    tes

---
+++ Ts.java
@@ -0,0 +1,12 @@
+public class Ts{
+    
+    public static void jumlahArray(double[] array1, double[] array2){
+        int i;
+        for(i=0;i<array1.length;i++){
+            
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
