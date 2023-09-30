d6dbcf5b947245102d875a7b06fefa062adb22fe
zaki <zakiyudhistira.itb69@gmail.com>
2023-10-01 03:54:11 +0700

    tested matrix

---
+++ Tes_matriks.java
@@ -0,0 +1,7 @@
+public class Tes_matriks {
+    public static void main(String args[]){
+        double[][] ini = new double[100][100];
+        System.out.println(ini.length);
+        System.out.println(ini[0].length);
+    }
+}
